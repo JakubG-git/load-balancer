e97e4182064bcd2201e554a45070887c42920363
Michał Koczkodaj <mkoczkodaj01@gmail.com>
2024-01-09 20:34:03 +0100

    feat(logger & lbStrategy): Added lbStrategy and logger

---
+++ src/main/kotlin/LoggerDB/LoggerDB.kt
@@ -0,0 +1,20 @@
+package logging
+
+import java.util.*
+import java.util.logging.ConsoleHandler
+import java.util.logging.Logger
+
+object LoggerDB {
+    private val LOGGERS: MutableMap<Class<*>, Logger?> = HashMap()
+    fun getLogger(c: Class<*>): Logger? {
+        var logger = LOGGERS[c]
+        if (logger != null) return logger
+        val handler = ConsoleHandler()
+        handler.formatter = FormatterDB()
+        logger = Logger.getLogger(c.name)
+        logger.useParentHandlers = false
+        logger.addHandler(handler)
+        LOGGERS.put(c, logger)
+        return logger
+    }
+}
