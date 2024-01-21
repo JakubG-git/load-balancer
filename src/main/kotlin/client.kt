4af06cd4068976b885f9a636ba81b155eacceafc
Michał Koczkodaj <92547392+mkoczkodaj@users.noreply.github.com>
2024-01-21 19:32:09 +0100

    dodanie modelu

---
+++ src/main/kotlin/client.kt
@@ -0,0 +1,25 @@
+package Model
+
+data class Client(
+    var idClient: Int,
+    var name: String,
+    var street: String,
+    var city: String,
+    var postalCode: String,
+    var phone: String
+) {
+    // Method to update the address of the client
+    fun updateAddress(newStreet: String, newCity: String, newPostalCode: String) {
+        this.street = newStreet
+        this.city = newCity
+        this.postalCode = newPostalCode
+    }
+
+    // Method to print the client details
+    fun printDetails() {
+        println("Client ID: $idClient")
+        println("Name: $name")
+        println("Address: $street, $city, $postalCode")
+        println("Phone: $phone")
+    }
+}
