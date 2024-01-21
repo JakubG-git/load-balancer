c5d5ec349bf25f98e3282a3b352456e7caa6389a
Michał Koczkodaj <92547392+mkoczkodaj@users.noreply.github.com>
2024-01-21 19:44:43 +0100

    dodanie adnotacji hibernate'a

--- src/main/kotlin/client.kt
+++ src/main/kotlin/client.kt
@@ -1,10 +1,19 @@
+@Entity
+@Table(name = "clients")
 data class Client(
-    var idClient: Int,
-    var name: String,
-    var street: String,
-    var city: String,
-    var postalCode: String,
-    var phone: String
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(name = "name", nullable = false)
+    val name: String,
+
+    @Column(name = "address")
+    val address: String,
+
+    @Column(name = "phone")
+    val phone: String
+
 ) {
     // Method to update the address of the client
     fun updateAddress(newStreet: String, newCity: String, newPostalCode: String) {
