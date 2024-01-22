f7adc4f55317afae11971d04fd92de3817e72ac6
luklabuz <92466711+luklabuz@users.noreply.github.com>
2024-01-22 12:15:11 +0100

    Delete src/main/kotlin/LoadBalancer/LoadBalancingMechanism/LoadBalancingMechanism.kt
    prunin'

--- src/main/kotlin/LoadBalancer/LoadBalancingMechanism/LoadBalancingMechanism.kt
+++
@@ -1,7 +0,0 @@
-package LoadBalancer.LoadBalancingMechanism
-
-import LoadBalancer.Session.LoadBalancingSession
-
-interface LoadBalancingMechanism<T> {
-    fun get(sessions: List<LoadBalancingSession<T>>): LoadBalancingSession<T>
-}
