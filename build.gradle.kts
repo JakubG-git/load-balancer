448f4f45415f0d823ad9fd7734cd3feba2f66b3f
JakubG-git <jakub.macius@gmail.com>
2024-01-22 19:59:27 +0100

    database config files

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.hibernate.orm:hibernate-core:6.4.1.Final")
+    implementation("org.postgresql:postgresql:42.5.1")
 }
 
 tasks.test {
--- src/main/kotlin/loadBalancer/loadBalancingMechanism/WeightedResponseTime.kt
+++ src/main/kotlin/loadBalancer/loadBalancingMechanism/WeightedResponseTime.kt
@@ -4,10 +4,13 @@ import loadBalancer.session.LoadBalancingSession
 
 class WeightedResponseTime<T> : LoadBalancingMechanism<T> {
 
+    //    override fun get(sessions: List<LoadBalancingSession<T>>): LoadBalancingSession<T> {
+//        if (sessions.isEmpty()) throw IllegalStateException("No sessions available")
+//
+//        return sessions.minByOrNull { it.getAverageResponseTime() ?: Double.MAX_VALUE }
+//            ?: throw IllegalStateException("No active session found")
+//    }
     override fun get(sessions: List<LoadBalancingSession<T>>): LoadBalancingSession<T> {
-        if (sessions.isEmpty()) throw IllegalStateException("No sessions available")
-
-        return sessions.minByOrNull { it.getAverageResponseTime() ?: Double.MAX_VALUE }
-            ?: throw IllegalStateException("No active session found")
+        TODO("Not yet implemented")
     }
 }
